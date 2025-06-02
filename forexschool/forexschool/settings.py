604b6190ef85ba3a5806248f3573c85cfb5e5735
samprinc <samprinc1122@gmail.com>
2025-06-02 12:44:11 +0300

    Update settings.py

--- forexschool/forexschool/settings.py
+++ forexschool/forexschool/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-4pqh*b$$^9+s4krp5a0uy@07bc2m_4wbl9p10f+5^d7p%@mdq*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
